1fb8e5ef31e0425784fcb834b55bf074c145f908
Coder San <codersan78@gmail.com>
2019-04-05 21:32:45 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,18 @@
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10,b=20;
+		int temp;
+		System.out.println("a="+a+" b="+b);	//before swapping
+		
+		temp = a;	//Value of temp = 3
+		a = b;		// a = 5
+		b = temp;	//b = 3
+		System.out.println("a="+a+" b="+b);	//after swapping
+		
+		
+
+	}
+
+}
