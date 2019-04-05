a64010dee99bc82ff0f054e67e3bb15c06bb4e8c
Coder San <codersan78@gmail.com>
2019-04-05 13:58:00 +0530

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter two numbers: ");
+		int a=5,b=4;
+		System.out.println("The sum is: "+(a+b));
+		
+		
+
+	}
+
+}
