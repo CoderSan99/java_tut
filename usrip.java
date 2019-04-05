61af30f17f93b9ece9375cbd06e04ffd0f74b93f
Coder San <codersan78@gmail.com>
2019-04-05 15:47:02 +0530

    Create usrip.java

---
+++ usrip.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class usrip {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers: ");
+		float a = sc.nextFloat(); 
+		float b = sc.nextFloat();
+		System.out.println("The sum is: "+(a+b));
+		sc.close();
+		
+		
+
+	}
+
+}
